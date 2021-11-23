6183b3e93a5b6defe551af1f9f4fefe0593796a3
Nico88888888 <92450360+Nico88888888@users.noreply.github.com>
2021-11-23 23:50:37 +0200

    Update fisier.cpp

--- fisier.cpp
+++ fisier.cpp
@@ -2,10 +2,6 @@
 using namespace std;
 int main()
 {
-  cout<<1<<" ";
-  cout<<2<<" ";
-  cout<<3<<" ";
-  cout<<4<<" ";
-  cout<<5<<" ";
+  cout<<1<<" "<<2<<" "<<3<<" "<<4<<" "<<5<<" ";
   return 0;
 }
